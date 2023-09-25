64675246d92d3c442e4acaa7d49339fa66babe3c
Netdiebeste <134294640+Netdiebeste@users.noreply.github.com>
2023-09-25 16:01:09 +0200

    Add files via upload

---
+++ CrimBot/CRIMBOT.py
@@ -0,0 +1,18 @@
+#this is a simple script that locates the "claim allowance" button and auto clicks it. 
+#you must make sure the click location is set SPECIFICALLY for you (on the claim allowance button)
+#use an app like https://sourceforge.net/projects/mpos/ to find the coordinates and put the x value and y value in is appropriate position
+
+from pyautogui import * 
+import pyautogui 
+import time 
+import keyboard 
+import random
+import win32api, win32con
+
+while 1:
+    if pyautogui.locateOnScreen('button.png', confidence=0.8) != None:
+        pyautogui.click(692, 500), print("ALLOWANCE SUCCESFULLY CLAIMED")
+        time.sleep(0.5)
+    else:
+        print("please wait")
+        time.sleep(0.5)
